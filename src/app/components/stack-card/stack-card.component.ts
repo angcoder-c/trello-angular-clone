c1dccf6f34167e4321d69ceab3e6f0e1f226ea7d
angcoder-c <angcoder.c@gmail.com>
2026-01-25 14:41:09 -0600

    feat: add stack card componenet

---
+++ src/app/components/stack-card/stack-card.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-stack-card',
+  imports: [],
+  templateUrl: './stack-card.component.html',
+  styleUrl: './stack-card.component.css'
+})
+export class StackCardComponent {
+  readonly cardUrl = input.required<string>();
+  readonly cardName = input.required<string>();
+}
