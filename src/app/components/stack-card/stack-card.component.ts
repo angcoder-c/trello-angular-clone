8e1d898ac910d270f56778c7160737cf7a1c3470
angcoder-c <angcoder.c@gmail.com>
2026-01-25 14:45:04 -0600

    fix: update links card

--- src/app/components/stack-card/stack-card.component.ts
+++ src/app/components/stack-card/stack-card.component.ts
@@ -1,8 +1,9 @@
 import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-stack-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './stack-card.component.html',
   styleUrl: './stack-card.component.css'
 })
