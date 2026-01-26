beac67b502c71fd059503f1dd98e8aa42a7cd670
angcoder-c <angcoder.c@gmail.com>
2026-01-25 19:40:45 -0600

    feat: add not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { boardResolver } from './routing/resolvers';
 import { BoardsComponent } from './pages/boards/boards.component';
 import { AboutComponent } from './pages/about/about.component';
 import { AuthCallbackComponent } from './auth/auth-callback/auth-callback.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     {
@@ -39,5 +40,10 @@ export const routes: Routes = [
         resolve: {
             board: boardResolver
         }
+    },
+    {
+        path: '**',
+        title: '404 | Page not found',
+        component: NotFoundComponent
     }
 ];
